67d3f5d2c1d3c7f421fd1d55827c4aff6e83cd10
Martin Etmajer <martin.etmajer@boehringer-ingelheim.com>
2019-12-30 15:22:35 +0100

    Add /health endpoint (#5)
    * Add /health endpoint
    * Add Kubernetes liveness probe
    
    Co-authored-by: Martin Etmajer <martin.etmajer@boehringer-ingelheim.com>

--- src/main/groovy/app/App.groovy
+++ src/main/groovy/app/App.groovy
@@ -4,10 +4,13 @@ import com.typesafe.config.ConfigFactory
 
 import groovy.json.JsonSlurper
 
+import java.nio.file.Files
+
 import org.jooby.Jooby
 import org.jooby.MediaType
 import org.jooby.json.Jackson
 
+import static org.jooby.JoobyExtension.get
 import static org.jooby.JoobyExtension.post
 
 class App extends Jooby {
@@ -28,6 +31,48 @@ class App extends Jooby {
             ])
         })
         .consumes(MediaType.json)
+        .produces(MediaType.json)
+
+        get(this, "/health", { req, rsp ->
+            def documentHtmlFile = Files.createTempFile("document", ".html") << "<html>document</html>"
+            def headerHtmlFile = Files.createTempFile("header", ".html") << "<html>header</html>"
+            def footerHtmlFile = Files.createTempFile("footer", ".html") << "<html>footer</html>"
+
+            def message = null
+            def status = "passing"
+            def statusCode = 200
+
+            def data
+            try {
+                data = DocGen.Util.convertHtmlToPDF(documentHtmlFile, headerHtmlFile, footerHtmlFile)
+            } catch (e) {
+                message = e.message
+                status = "failing"
+                statusCode = 500
+            } finally {
+                Files.delete(documentHtmlFile)
+                Files.delete(headerHtmlFile)
+                Files.delete(footerHtmlFile)
+            }
+
+            if (!new String(data).startsWith("%PDF-1.4\n")) {
+                message = "conversion form HTML to PDF failed"
+                status = "failing"
+                statusCode = 500
+            }
+
+            def result = [
+                service: "docgen",
+                status: status,
+                time: new Date().toString()
+            ]
+
+            if (message) {
+                result.message = message
+            }
+
+            rsp.status(statusCode).send(result)
+        })
         .produces(MediaType.json)
     }
 
